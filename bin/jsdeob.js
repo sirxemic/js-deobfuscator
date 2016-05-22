39ac469635ac00887b5323314bf6d5ae32722247
Pim Schreurs <pimschreurs@gmail.com>
2016-05-22 21:29:33 +0200

    Add options to binary and refactor a bit

--- bin/jsdeob.js
+++ bin/jsdeob.js
@@ -4,19 +4,26 @@
 var path = require('path');
 var fs = require('fs');
 
-var jsDeob = require('../index');
+var prettify = require('../lib').prettify;
 
+/**
+ *  Show usage and exit.
+ */
 function help(status) {
   var print = status == 0 ? console.log : console.error;
 
-  print('usage: ' + path.basename(process.argv[1]) + ' [--help] [--] [infile]');
+  print('Usage: ' + path.basename(process.argv[1]) + ' [--ecma3|--ecma5|--ecma6|--ecma7]');
+  print('       [--indent=NUM] [--help] [--] [infile]');
 
   process.exit(status);
 }
 
+/**
+ *  Run the prettifier.
+ */
 function run(code, options) {
   try {
-    var result = jsDeob(code);
+    var result = prettify(code, options);
     console.log(result);
   }
   catch (e) {
@@ -25,11 +32,17 @@ function run(code, options) {
   }
 }
 
+/**
+ *  Run the prettifier using a file as input.
+ */
 function runFromFile(file, options) {
   var code = fs.readFileSync(file, 'utf8');
   run(code, options);
 }
 
+/**
+ *  Run the prettifier using stdin as input.
+ */
 function runFromStdin(options) {
   var code = '';
 
@@ -43,24 +56,60 @@ function runFromStdin(options) {
   });
 }
 
+/**
+ *  Parse and return the prettifier options from the process arguments.
+ */
 function parseArguments() {
   var options = {};
 
   for (var i = 2; i < process.argv.length; i++) {
-    var arg = process.argv[i];
+    var m, arg = process.argv[i];
     if ((arg == '-' || arg[0] != '-') && !options.file) {
       options.file = arg;
     }
     else if (arg == '--' && !options.file && i + 2 == process.argv.length) {
       options.forceFile = options.file = process.argv[++i];
     }
-    else if (arg == '--help') help(0);
-    else help(1);
+    else if (arg == "--ecma3") {
+      options.ecmaVersion = 3;
+    }
+    else if (arg == "--ecma5") {
+      options.ecmaVersion = 5;
+    }
+    else if (arg == "--ecma6") {
+      options.ecmaVersion = 6;
+    }
+    else if (arg == "--ecma7") {
+      options.ecmaVersion = 7;
+    }
+    else if (arg == "--indent" && i + 1 < process.argv.length) {
+      var indent = parseInt(process.argv[++i]);
+      if (isNaN(indent)) {
+        help(1);
+      }
+      options.indent = indent;
+    }
+    else if ((m = arg.match(/--indent=(\d+)/))) {
+      var indent = parseInt(m[1]);
+      if (isNaN(indent)) {
+        help(1);
+      }
+      options.indent = indent;
+    }
+    else if (arg == '--help') {
+      help(0);
+    }
+    else {
+      help(1);
+    }
   }
 
   return options;
 }
 
+/**
+ *  Main.
+ */
 var options = parseArguments();
 
 if (options.forceFile || options.file && options.file != '-') {
